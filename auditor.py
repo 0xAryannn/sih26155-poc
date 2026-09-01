599b3725f88b8564d8ee7553d288a0b95be14fa6
Aryan Dhole <dholearyan2012@gmail.com>
2026-09-01 23:35:52 +0530

    Update auditor.py

--- auditor.py
+++ auditor.py
@@ -1,1 +1,24 @@
+config_bad = """
+hostname EDGE-R1
+enable password cisco123
+line vty 0 4
+ transport input telnet
+snmp-server community public RO
+no ip http server
+"""
 
+config_good = """
+hostname EDGE-R1
+enable password Str0ng!Pass
+line vty 0 4
+ transport input ssh
+snmp-server community Xk7#pQz RO
+service password-encryption
+no ip http server
+"""
+
+print("Bad configuration:")
+print(config_bad)
+
+print("Good configuration:")
+print(config_good)
